dbc0a91e811c678d71f8f5a160e12fcdf585572b
Ayan Banerjee <ayanbanerjee7777@gmail.com>
2019-05-14 17:25:53 +0530

    Tests for TCGA-OV, -COAD, -BLCA, -GBM

--- test.py
+++ test.py
@@ -1,3 +1,5 @@
+from __future__ import print_function
+
 import time
 
 from xena_gdc_etl import xena_dataset
@@ -9,41 +11,41 @@ from colorama import Fore, Style
 # end
 
 
-GDC_XENA_COHORT = {
-        'TCGA-BRCA': 'GDC TCGA Breast Cancer (BRCA)',
-        'TCGA-LUAD': 'GDC TCGA Lung Adenocarcinoma (LUAD)',
-        'TCGA-UCEC': 'GDC TCGA Endometrioid Cancer (UCEC)',
-        'TCGA-LGG': 'GDC TCGA Lower Grade Glioma (LGG)',
-        'TCGA-HNSC': 'GDC TCGA Head and Neck Cancer (HNSC)',
-        'TCGA-PRAD': 'GDC TCGA Prostate Cancer (PRAD)',
-        'TCGA-LUSC': 'GDC TCGA Lung Squamous Cell Carcinoma (LUSC)',
-        'TCGA-THCA': 'GDC TCGA Thyroid Cancer (THCA)',
-        'TCGA-SKCM': 'GDC TCGA Melanoma (SKCM)',
-        'TCGA-OV': 'GDC TCGA Ovarian Cancer (OV)',
-        'TCGA-STAD': 'GDC TCGA Stomach Cancer (STAD)',
-        'TCGA-COAD': 'GDC TCGA Colon Cancer (COAD)',
-        'TCGA-BLCA': 'GDC TCGA Bladder Cancer (BLCA)',
-        'TCGA-GBM': 'GDC TCGA Glioblastoma (GBM)',
-        'TCGA-LIHC': 'GDC TCGA Liver Cancer (LIHC)',
-        'TCGA-KIRC': 'GDC TCGA Kidney Clear Cell Carcinoma (KIRC)',
-        'TCGA-CESC': 'GDC TCGA Cervical Cancer (CESC)',
-        'TCGA-KIRP': 'GDC TCGA Kidney Papillary Cell Carcinoma (KIRP)',
-        'TCGA-SARC': 'GDC TCGA Sarcoma (SARC)',
-        'TCGA-ESCA': 'GDC TCGA Esophageal Cancer (ESCA)',
-        'TCGA-PAAD': 'GDC TCGA Pancreatic Cancer (PAAD)',
-        'TCGA-PCPG': 'GDC TCGA Pheochromocytoma & Paraganglioma (PCPG)',
-        'TCGA-READ': 'GDC TCGA Rectal Cancer (READ)',
-        'TCGA-TGCT': 'GDC TCGA Testicular Cancer (TGCT)',
-        'TCGA-LAML': 'GDC TCGA Acute Myeloid Leukemia (LAML)',
-        'TCGA-THYM': 'GDC TCGA Thymoma (THYM)',
-        'TCGA-ACC': 'GDC TCGA Adrenocortical Cancer (ACC)',
-        'TCGA-MESO': 'GDC TCGA Mesothelioma (MESO)',
-        'TCGA-UVM': 'GDC TCGA Ocular melanomas (UVM)',
-        'TCGA-KICH': 'GDC TCGA Kidney Chromophobe (KICH)',
-        'TCGA-UCS': 'GDC TCGA Uterine Carcinosarcoma (UCS)',
-        'TCGA-CHOL': 'GDC TCGA Bile Duct Cancer (CHOL)',
-        'TCGA-DLBC': 'GDC TCGA Large B-cell Lymphoma (DLBC)'
-    }
+GDC_XENA_COHORT = [
+    'TCGA-BRCA', #
+    'TCGA-LUAD', #
+    'TCGA-UCEC', #
+    'TCGA-LGG', #
+    'TCGA-HNSC', #
+    'TCGA-PRAD', #
+    'TCGA-LUSC', #
+    'TCGA-THCA', #
+    'TCGA-SKCM', #
+    'TCGA-OV', #
+    'TCGA-STAD', #
+    'TCGA-COAD', #
+    'TCGA-BLCA', #
+    'TCGA-GBM', #
+    'TCGA-LIHC',
+    'TCGA-KIRC',
+    'TCGA-CESC',
+    'TCGA-KIRP',
+    'TCGA-SARC',
+    'TCGA-ESCA',
+    'TCGA-PAAD',
+    'TCGA-PCPG',
+    'TCGA-READ',
+    'TCGA-TGCT',
+    'TCGA-LAML',
+    'TCGA-THYM',
+    'TCGA-ACC',
+    'TCGA-MESO',
+    'TCGA-UVM',
+    'TCGA-KICH', #
+    'TCGA-UCS',
+    'TCGA-CHOL',
+    'TCGA-DLBC',
+]
 
 
 xena_dtypes = [
@@ -63,9 +65,13 @@ xena_dtypes = [
 # testing code starts here
 import sys
 if sys.version_info[0] < 3:
-    projects = list(GDC_XENA_COHORT.keys())[9]  # TCGA-OV
-else:
-    projects = list(GDC_XENA_COHORT.keys())[10]  # TCGA-STAD
+    projects = GDC_XENA_COHORT[9]  # TCGA-OV
+elif sys.version_info[0] == 3 and sys.version_info[1] == 7:
+    projects = GDC_XENA_COHORT[11]  # TCGA-COAD
+elif sys.version_info[0] == 3 and sys.version_info == 6:
+    projects = GDC_XENA_COHORT[12]  # TCGA-BLCA
+elif sys.version_info[0] == 3 and sys.version_info == 5:
+    projects = GDC_XENA_COHORT[13]  # TCGA-GBM
 for xena_dtype in xena_dtypes:
     try:
         start = time.time()
